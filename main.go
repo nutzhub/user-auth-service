c95446d8a5b03c3588e4619ed1fcd0a395630163
nut-abctech <Nut@abctech-thailand.com>
2015-04-07 12:15:04 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "server ready")
+	})
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "waiting for implementation", "status": http.StatusOK})
+	})
+	r.GET("/users/:id", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "waiting for implementation", "status": http.StatusOK})
+	})
+
+	r.GET("/users/:id/activities", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "waiting for implementation", "status": http.StatusOK})
+	})
+
+	r.POST("/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "waiting for implementation", "status": http.StatusOK})
+	})
+
+	r.POST("/users/auth", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "waiting for implementation", "status": http.StatusOK})
+	})
+
+	log.Fatal(r.Run(":8080"))
+}
